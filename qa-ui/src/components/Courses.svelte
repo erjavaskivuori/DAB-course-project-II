ff7e4cf9497ebb35ee01fe21fe6aa2deab2c4aee
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-30 21:59:37 +0200

    base for displaying courses

---
+++ qa-ui/src/components/Courses.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from "svelte";
+
+  let courses = [];
+
+  const getCourses = async () => {
+    const response = await fetch("/api/courses");
+    courses = await response.json();
+    console.log(courses);
+  };
+
+  onMount(() => {
+    getCourses();
+  });
+</script>
+
+<div>
+  <h1>Courses</h1>
+  <ul>
+    {#each courses as course}
+      <li>{course.title}</li>
+    {/each}
+  </ul>
+</div>
