c58a11d8edb5b5aa77d93f99ca3b350e50a9f450
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-02-03 17:49:50 +0200

    joined quering course and its questions, created query to fetch question and its answers

--- qa-api/services/courseService.js
+++ qa-api/services/courseService.js
@@ -4,12 +4,32 @@ const getAllCourses = async () => {
   return await sql `SELECT * FROM courses`;
 };
 
-const getCourse = async (courseId) => {
-  return await sql `SELECT * FROM courses WHERE id = ${courseId}`;
-}
-
-const getCourseQuestions = async (courseId) => {
-  return await sql `SELECT * FROM questions WHERE course_id = ${courseId}`;
+const getCourseWithQuestions = async (courseId) => {
+  return await sql`
+    SELECT 
+      courses.id AS course_id,
+      courses.title AS course_title,
+      questions.id AS question_id,
+      questions.content AS question_content,
+      questions.last_updated AS question_last_updated
+    FROM courses
+    LEFT JOIN questions ON courses.id = questions.course_id
+    WHERE courses.id = ${courseId}
+  `;
 };
 
-export { getAllCourses, getCourse, getCourseQuestions };
+const getQuestionWithAnswers = async (questionId) => {
+  return await sql`
+    SELECT 
+      questions.id AS question_id,
+      questions.content AS question_content,
+      answers.id AS answer_id,
+      answers.content AS answer_content,
+      answers.last_updated AS answer_last_updated
+    FROM questions
+    LEFT JOIN answers ON questions.id = answers.question_id
+    WHERE questions.id = ${questionId}
+  `;
+};
+
+export { getAllCourses, getCourseWithQuestions, getQuestionWithAnswers };
