f54a5d77a411144915ebdf4ef94ae5dc322d22d9
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-02-08 21:02:07 +0200

    created functionality for posting upvote and updating the context after

--- qa-ui/src/components/Course.svelte
+++ qa-ui/src/components/Course.svelte
@@ -1,3 +1,8 @@
+<script context="module">
+  import { setContext } from "svelte";
+  export const update = () => {};
+</script>
+
 <script>
   import { onMount } from "svelte";
   import { get } from "svelte/store";
@@ -5,7 +10,7 @@
   import UpvoteButton from "./UpvoteButton.svelte";
 
   export let courseId;
-  let course;
+  let course = {};
   let questionInput = "";
 
   const getCourse = async () => {
@@ -31,46 +36,50 @@
     const jsonData = await response.json();
     console.log(jsonData);
     questionInput = "";
-    getCourse();
+    await getCourse();
   };
 
   onMount(() => {
     getCourse();
   });
+
+  setContext("update", getCourse);
 </script>
 
 <div class="px-6">
-  {#if course}
-    <h1 class="text-3xl my-6">{course.title}</h1>
-    <label for="question">Ask a question:</label>
-    <div class="flex flex-row my-3">
-      <input bind:value={questionInput}
-        type="text"
-        id="question"
-        class="block w-full p-2 border border-gray-200 rounded-lg shadow-sm mr-2" />
-      <button on:click={postQuestion}
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1.5 rounded-lg">
-        Send
-      </button>
-    </div>
-    <h2 class="text-xl mt-7 mb-4">Questions:</h2>
-    {#if course.questions}
-      <ul>
-        {#each course.questions as question (question.id)}
-          <li>
-            <div class="flex flex-col items-start justify-between my-4">
-              <a href="/questions/{question.id}" class="block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 mb-2">
-                {question.content}
-              </a>
-              <UpvoteButton upvotes={question.upvoted_by} />
-            </div>
-          </li>
-        {/each}
-      </ul>
-    {:else}
-      <p>
-        No questions yet, be the first to ask!
-      </p>
+  {#key course}
+    {#if course}
+      <h1 class="text-3xl my-6">{course.title}</h1>
+      <label for="question">Ask a question:</label>
+      <div class="flex flex-row my-3">
+        <input bind:value={questionInput}
+          type="text"
+          id="question"
+          class="block w-full p-2 border border-gray-200 rounded-lg shadow-sm mr-2" />
+        <button on:click={postQuestion}
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1.5 rounded-lg">
+          Send
+        </button>
+      </div>
+      <h2 class="text-xl mt-7 mb-4">Questions:</h2>
+      {#if course.questions}
+        <ul>
+          {#each course.questions as question (question.id)}
+            <li>
+              <div class="flex flex-col items-start justify-between my-4">
+                <a href="/questions/{question.id}" class="block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 mb-2">
+                  {question.content}
+                </a>
+                <UpvoteButton upvotes={question.upvoted_by} objectType="question" objectId={question.id} />
+              </div>
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <p>
+          No questions yet, be the first to ask!
+        </p>
+      {/if}
     {/if}
-  {/if}
+  {/key}
 </div>
--- qa-ui/src/components/Question.svelte
+++ qa-ui/src/components/Question.svelte
@@ -1,3 +1,8 @@
+<script context="module">
+  import { setContext } from "svelte";
+  export const update = () => {};
+</script>
+
 <script>
   import { onMount } from "svelte";
   import { get } from "svelte/store";
@@ -6,7 +11,7 @@
 
   export let questionId;
   let answerInput = "";
-  let question;
+  let question = {};
 
   const getQuestion = async () => {
     const response = await fetch(`/api/question?id=${questionId}`);
@@ -31,48 +36,52 @@
     const jsonData = await response.json();
     console.log(jsonData);
     answerInput = "";
-    getQuestion();
+    await getQuestion();
   };
 
   onMount(() => {
     getQuestion();
   });
+
+  setContext("update", getQuestion);
 </script>
 
 <div class="flex w-full h-full flex-row items-center justify-center">
   <div class="sm:max-w-2xl lg:max-w-4xl px-6">
-    {#if question}
-      <h1 class="text-3xl my-6">Question: {question.content}</h1>
-      <label for="answer">Write your answer:</label>
-      <div class="flex flex-row my-3">
-        <input bind:value={answerInput}
-          type="text"
-          id="answer"
-          class="block w-full p-2 border border-gray-200 rounded-lg shadow-sm mr-2"/>
-        <button on:click={postAnswer}
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1.5 rounded-lg">
-          Send
-        </button>
-      </div>
-      <h2 class="text-2xl mt-7 mb-4">Answers:</h2>
-      {#if question.answers && question.answers[0].id !== null}
-        <ul>
-          {#each question.answers as answer (answer.id)}
-            <li>
-              <div class="flex flex-col items-start justify-between my-4">
-                <p class="block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow-sm mb-2">
-                  {answer.content}
-                </p>
-                <UpvoteButton upvotes={answer.upvoted_by} />
-              </div>
-            </li>
-          {/each}
-        </ul>
-      {:else}
-        <p>
-          No answers yet, be the first to answer!
-        </p>
+    {#key question}
+      {#if question}
+        <h1 class="text-3xl my-6">Question: {question.content}</h1>
+        <label for="answer">Write your answer:</label>
+        <div class="flex flex-row my-3">
+          <input bind:value={answerInput}
+            type="text"
+            id="answer"
+            class="block w-full p-2 border border-gray-200 rounded-lg shadow-sm mr-2"/>
+          <button on:click={postAnswer}
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1.5 rounded-lg">
+            Send
+          </button>
+        </div>
+        <h2 class="text-2xl mt-7 mb-4">Answers:</h2>
+        {#if question.answers && question.answers[0].id !== null}
+          <ul>
+            {#each question.answers as answer (answer.id)}
+              <li>
+                <div class="flex flex-col items-start justify-between my-4">
+                  <p class="block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow-sm mb-2">
+                    {answer.content}
+                  </p>
+                  <UpvoteButton upvotes={answer.upvoted_by} objectType="answer" objectId={answer.id} />
+                </div>
+              </li>
+            {/each}
+          </ul>
+        {:else}
+          <p>
+            No answers yet, be the first to answer!
+          </p>
+        {/if}
       {/if}
-    {/if}
+    {/key}
   </div>
 </div>
