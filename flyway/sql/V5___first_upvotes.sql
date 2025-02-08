1b162c54f62bb3141ad2d1c9004fa2be026ffe87
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-02-08 19:08:04 +0200

    added initial upvotes

---
+++ flyway/sql/V5___first_upvotes.sql
@@ -0,0 +1,15 @@
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (1, 'question', 'user3');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (1, 'question', 'user2');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (1, 'question', 'user1');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (3, 'question', 'user1');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (2, 'answer', 'user3');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (2, 'answer', 'user2');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (3, 'answer', 'user2');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (4, 'answer', 'user1');
