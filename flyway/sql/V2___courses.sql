5b01477b24285f13526373798f214e9cf1f30acc
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-02-11 20:58:31 +0200

    updated sample data

--- flyway/sql/V2___courses.sql
+++ flyway/sql/V2___courses.sql
@@ -4,8 +4,4 @@ INSERT INTO courses (title) VALUES ('Software Architectures');
 
 INSERT INTO courses (title) VALUES ('Software Development Process');
 
-INSERT INTO courses (title) VALUES ('Requirements Engineering');
-
 INSERT INTO courses (title) VALUES ('Full Stack Open');
-
-INSERT INTO courses (title) VALUES ('Software Evolution and Reliability');
--- flyway/sql/V5___first_upvotes.sql
+++ flyway/sql/V5___first_upvotes.sql
@@ -6,6 +6,10 @@ INSERT INTO upvotes (object_id, object_type, user_id) VALUES (1, 'question', 'us
 
 INSERT INTO upvotes (object_id, object_type, user_id) VALUES (3, 'question', 'user1');
 
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (3, 'question', 'user2');
+
+INSERT INTO upvotes (object_id, object_type, user_id) VALUES (4, 'question', 'user3');
+
 INSERT INTO upvotes (object_id, object_type, user_id) VALUES (2, 'answer', 'user3');
 
 INSERT INTO upvotes (object_id, object_type, user_id) VALUES (2, 'answer', 'user2');
